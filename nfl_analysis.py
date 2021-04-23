f00685a4de74b87b3f533bf29b3d1aec2ab301c8
ZeNoonan <darragh.noonan@gmail.com>
2021-04-23 15:40:43 +0100

    update

--- nfl_analysis.py
+++ nfl_analysis.py
@@ -2,10 +2,16 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+st.set_page_config(layout="wide")
 data = pd.read_excel('C:/Users/Darragh/Documents/Python/NFL/NFL_2019_Data.xlsx')
 
-st.table(data.head())
+# st.table(data.head())
+data['home_win']=data['Home Points'] - data['Away Points']
+data['home_win'] = np.where((data['Home Points'] > data['Away Points']), 1, np.where((data['Home Points'] < data['Away Points']),-1,0))
+data['home_cover']=np.where(((data['Home Points'] + data['Spread']) > data['Away Points']), 1, np.where(((data['Home Points']+ data['Spread']) < data['Away Points']),-1,0))
+data['home_season_cover'] = data.groupby (['Home ID'])['home_cover'].cumsum()
 
+st.dataframe(data)
 # here we go, create a column with Last Game Turnover
 # create the season to date cover for every team
 # data['Home_Last_Turnover']=test test TEST
