979df548abf836c4b30bd7014a3ff4700cb5901c
ZeNoonan <darragh.noonan@gmail.com>
2021-04-25 08:10:55 +0100

    update season cover

--- nfl_analysis.py
+++ nfl_analysis.py
@@ -5,39 +5,48 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 @st.cache
-def read_data():
-    return pd.read_excel('C:/Users/Darragh/Documents/Python/NFL/NFL_2019_Data.xlsx') 
-data = read_data()
+def read_data(file):
+    return pd.read_excel(file) 
+data_2019 = read_data('C:/Users/Darragh/Documents/Python/NFL/NFL_2019_Data.xlsx').copy()
+data_2020=read_data('C:/Users/Darragh/Documents/Python/NFL/NFL_2020_Data.xlsx').copy()
 
 # st.table(data.head())
-data['home_win']=data['Home Points'] - data['Away Points']
-data['home_win'] = np.where((data['Home Points'] > data['Away Points']), 1, np.where((data['Home Points'] < data['Away Points']),-1,0))
-data['home_cover']=np.where(((data['Home Points'] + data['Spread']) > data['Away Points']), 1, np.where(((data['Home Points']+ data['Spread']) < data['Away Points']),-1,0))
-# data['home_season_cover'] = data.groupby (['Home ID'])['home_cover'].cumsum()
-season_cover_df=(data.set_index('Week').loc[0:,:]).copy()
-new = season_cover_df.loc[:,['Home ID','Away ID','home_cover']].copy()
-# st.write(new)
-test=new.reset_index()
-# st.write(test)
-test_test = pd.melt(test,id_vars=['Week', 'home_cover'],value_vars=['Home ID', 'Away ID']).set_index('Week').rename(columns={'value':'ID'}).drop('variable',axis=1).reset_index().sort_values(by=['Week','ID'],ascending=True)
-# test_test=set_index('Week').drop('index',axis=1)
-# st.write(test_test) #Looks like this works
+def spread_workings(data):
+    data['home_win']=data['Home Points'] - data['Away Points']
+    data['home_win'] = np.where((data['Home Points'] > data['Away Points']), 1, np.where((data['Home Points'] < data['Away Points']),-1,0))
+    data['home_cover']=np.where(((data['Home Points'] + data['Spread']) > data['Away Points']), 1,
+    np.where(((data['Home Points']+ data['Spread']) < data['Away Points']),-1,0))
+    data['away_cover'] = -data['home_cover']
+    return data
 
-test_test['home_season_cover'] = test_test.groupby (['ID'])['home_cover'].transform(lambda x: x.cumsum().shift())
-st.write(test_test)
-st.write(test_test.reset_index().sort_values(by=['Week','ID'],ascending=True))
-# season_cover_df['home_season_cover'] = season_cover_df.groupby (['Home ID'])['home_cover'].transform(lambda x: x.cumsum().shift())
-# https://stackoverflow.com/questions/54993050/pandas-groupby-shift-and-cumulative-sum
-# don't think this is going to work, need away season to date cover as well need a specific df is what i'm thinking
+spread=spread_workings(data_2020)
+# st.write(spread)
 
+def season_cover_workings(data):
+    season_cover_df=(data.set_index('Week').loc[1:,:]).reset_index()
+    home_cover_df = (season_cover_df.loc[:,['Week','Home ID','home_cover']]).rename(columns={'Home ID':'ID','home_cover':'cover'})
+    away_cover_df = (season_cover_df.loc[:,['Week','Away ID','away_cover']]).rename(columns={'Away ID':'ID','away_cover':'cover'})
+    season_cover=pd.concat([home_cover_df,away_cover_df],ignore_index=True)
+    # season_cover_df = pd.melt(season_cover_df,id_vars=['Week', 'home_cover'],value_vars=['Home ID', 'Away ID']).set_index('Week').rename(columns={'value':'ID'}).\
+    # drop('variable',axis=1).reset_index().sort_values(by=['Week','ID'],ascending=True)
+    return season_cover.sort_values(by=['Week','ID'],ascending=['True','True'])
 
+spread_1 = season_cover_workings(spread)
 
-st.dataframe(data)
+def season_cover_2(season_cover_df):    
+    # https://stackoverflow.com/questions/54993050/pandas-groupby-shift-and-cumulative-sum
+    season_cover_df['cover'] = season_cover_df.groupby (['ID'])['cover'].transform(lambda x: x.cumsum().shift())
+    season_cover_df=season_cover_df.reset_index().sort_values(by=['Week','ID'],ascending=True).drop('index',axis=1)
+    # Be careful with this if you want full season, season to date cover, for week 17, it is season to date up to week 16
+    # if you want full season, you have to go up to week 18 to get the full 17 weeks, just if you want to do analysis on season covers
 
-st.write(season_cover_df)
+    return season_cover_df
 
-# temp = pd.DataFrame(data=[['a',1],['a',1],['a',1],['b',1],['b',1],['b',1],['c',1],['c',1]], columns=['ID','X'])
-# st.write(temp)
-# here we go, create a column with Last Game Turnover
-# create the season to date cover for every team
-# data['Home_Last_Turnover']=test test TEST
+def season_cover_3(data):
+    data['cover_sign'] = np.where((data['cover'] > 0), 1, np.where((data['cover'] < 0),-1,0))
+    return data
+
+spread_2=season_cover_2(spread_1)
+st.write(spread_2.sort_values(by=['ID','Week'],ascending=['True','True']))
+spread_3=season_cover_3(spread_2)
+st.write(spread_3.sort_values(by=['ID','Week'],ascending=['True','True']))
