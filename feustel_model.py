5c77c55a736f5107c23d4e75f9df29473e047689
ZeNoonan <darragh.noonan@gmail.com>
2022-10-05 12:10:32 +0100

    update

--- feustel_model.py
+++ feustel_model.py
@@ -27,15 +27,20 @@ df['month'] = pd.DatetimeIndex(df['Date']).month
 df['season_month'] = df['month'].map({9:1,10:2,11:3,12:4,1:5,2:6})
 # NL_Raw_Clean['calendar_month']=NL_Raw_Clean['Per.'].map({1:9,2:10,3:11,4:12,5:1,6:2,7:3,8:4,9:5,10:6,11:7,12:8,19:8})
 df['season_year'] = np.where((df['season_month'] < 5), df['year'], df['year']-1)
+df['Home Team']=df['Home Team'].replace({'Washington Football Team':'Washington Commanders','Washington Redskins':'Washington Commanders','St. Louis Rams':'Los Angeles Rams',
+'Oakland Raiders':'Las Vegas Raiders','San Diego Chargers':'Los Angeles Chargers'})
+df['Away Team']=df['Away Team'].replace({'Washington Football Team':'Washington Commanders','Washington Redskins':'Washington Commanders','St. Louis Rams':'Los Angeles Rams',
+'Oakland Raiders':'Las Vegas Raiders','San Diego Chargers':'Los Angeles Chargers'})
 # NL_Raw_Clean['calendar_year']=NL_Raw_Clean['calendar_year']+2000
 # NL_Raw_Clean=NL_Raw_Clean.rename(columns={'calendar_year':'year', 'calendar_month':'month'})
 
 # df['']
-st.write(df)
+st.write(df.sort_values(by='Date'))
 # for _ in df.groupby('season_year'):
 #     pass
 
 df_offensive_home=df.loc[:,['Date','Home Team', 'Home Score', 'season_year']].rename(columns={'Home Team':'team','Home Score':'score'})
+
 df_offensive_home['home_away']=1
 df_offensive_away=df.loc[:,['Date','Away Team','Away Score', 'season_year']].rename(columns={'Away Team':'team','Away Score':'score'})
 df_offensive_away['home_away']=-1
@@ -50,7 +55,7 @@ df_offensive['sum_score']=df_offensive.groupby(['team','season_year'])['score'].
 df_offensive['mean_score']=df_offensive.groupby(['team','season_year'])['score'].rolling(window=4,min_periods=4, center=False).mean()\
     .reset_index().drop(['level_2','team','season_year'],axis=1)
 df_offensive=df_offensive.rename(columns={'score':'pts_scored','mean_score':'4_game_pts_scored'}).sort_values(by=['team','Date']).drop('sum_score',axis=1)
-st.write(df_offensive)
+# st.write(df_offensive)
 # df_groupby_scores=df_offensive.groupby(['team','season_year','Date'])['score'].rolling(window=4,min_periods=4, center=False).sum().reset_index()
 
 df_defensive_home=df.loc[:,['Date','Home Team', 'Away Score', 'season_year']].rename(columns={'Home Team':'team','Away Score':'score'})
@@ -66,6 +71,16 @@ df_defensive['sum_score']=df_defensive.groupby(['team','season_year'])['score'].
 df_defensive['mean_score']=df_defensive.groupby(['team','season_year'])['score'].rolling(window=4,min_periods=4, center=False).mean()\
     .reset_index().drop(['level_2','team','season_year'],axis=1)
 df_defensive=df_defensive.rename(columns={'score':'pts_conceded','mean_score':'4_game_pts_conceded'}).sort_values(by=['team','Date']).drop('sum_score',axis=1)
-st.write(df_defensive)
+# st.write(df_defensive)
 df_new=pd.merge(df_offensive,df_defensive,how='outer')
-st.write(df_new)
+df_new['team_cum_sum_pts']=df_new.groupby(['team'])['pts_scored'].cumsum()
+df_new['team_cum_sum_games']=df_new.groupby(['team'])['pts_scored'].cumcount()+1
+df_new['rolling_avg_team_pts_scored']=df_new['team_cum_sum_pts'] / df_new['team_cum_sum_games']
+df_new=df_new.sort_values(by=['Date','team'])
+df_new['date_avg_pts_rolling']=df_new['pts_scored'].expanding().mean()
+st.write('sorted by date avg score by date',df_new)
+df_new=df_new.sort_values(by=['team','Date'],ascending=True)
+st.write('after sorting ',df_new)
+
+st.write('sort out team names', df_new['team'].unique())
+st.write('calculate rolling avg home pts scored and home pts allowed')
